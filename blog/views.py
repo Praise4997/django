c0ed91014827b267839c434438bc633a3b8286a3
praise adedoyin <adedoyinpraise408@gmail.com>
2022-06-25 14:27:16 +0100

    a

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
+
+from django.views.generic.edit import CreateView
+from msilib.schema import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
